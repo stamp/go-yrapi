b100d69a3417b81dad2abee0995a24936a740b4e
stamp <stamp@stamp.se>
2018-12-31 16:11:33 +0100

    gofmt and new urls

--- yrapi/main.go
+++ yrapi/main.go
@@ -1,205 +1,205 @@
 package yrapi
 
-import(
-  "time"
-  "errors"
-  "fmt"
-  "encoding/xml"
-  "net/http"
-  "github.com/jonaz/astrotime"
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/jonaz/astrotime"
 )
 
 type WeatherModel struct {
-  Name string `xml:"name,attr"`
-  Termin string `xml:"termin,attr"`
-  RunEnded string `xml:"runended,attr"`
-  NextRun string `xml:"nextrun,attr"`
-  From string `xml:"from,attr"`
-  To string `xml:"to,attr"`
+	Name     string `xml:"name,attr"`
+	Termin   string `xml:"termin,attr"`
+	RunEnded string `xml:"runended,attr"`
+	NextRun  string `xml:"nextrun,attr"`
+	From     string `xml:"from,attr"`
+	To       string `xml:"to,attr"`
 }
 
 type WeatherMetadata struct {
-  Model WeatherModel `xml:"model"`
+	Model WeatherModel `xml:"model"`
 }
 
 type WeatherProduct struct {
-  ProductClass string `xml:"class,attr"`
-  WeatherTimes []WeatherTime `xml:"time"`
+	ProductClass string        `xml:"class,attr"`
+	WeatherTimes []WeatherTime `xml:"time"`
 }
 
 type WeatherData struct {
-  Created string `xml:"created,attr"`
-  Metadata WeatherMetadata `xml:"meta"`
-  Product WeatherProduct `xml:"product"`
+	Created  string          `xml:"created,attr"`
+	Metadata WeatherMetadata `xml:"meta"`
+	Product  WeatherProduct  `xml:"product"`
 }
 
 type WeatherTime struct {
-  Type string `xml:"datatype,attr"`
-  From time.Time `xml:"from,attr"`
-  To time.Time `xml:"to,attr"`
+	Type string    `xml:"datatype,attr"`
+	From time.Time `xml:"from,attr"`
+	To   time.Time `xml:"to,attr"`
 
-  Location WeatherLocation `xml:"location"`
+	Location WeatherLocation `xml:"location"`
 }
 
 type WeatherLocation struct {
-  Altitude int `xml:"altitude,attr"`
-  Latitude float64 `xml:"latitude,attr"`
-  Longitude float64 `xml:"longitude,attr"`
+	Altitude  int     `xml:"altitude,attr"`
+	Latitude  float64 `xml:"latitude,attr"`
+	Longitude float64 `xml:"longitude,attr"`
 
-  Precipitation *WeatherPrecipitation `xml:"precipitation"`
-  Symbol *WeatherSymbol `xml:"symbol"`
+	Precipitation *WeatherPrecipitation `xml:"precipitation"`
+	Symbol        *WeatherSymbol        `xml:"symbol"`
 
-  Temperature *WeatherTemperature `xml:"temperature,omitempty"`
-  WindDirection *WeatherWindDirection `xml:"windDirection,omitempty"`
-  WindSpeed *WeatherWindSpeed `xml:"windSpeed,omitempty"`
-  Humidity *WeatherHumidity `xml:"humidity,omitempty"`
-  Pressure *WeatherPressure `xml:"pressure,omitempty"`
-  Cloudiness *WeatherCloudiness `xml:"cloudiness,omitempty"`
-  Fog *WeatherFog `xml:"fog,omitempty"`
-  LowClouds *WeatherLowClouds `xml:"lowClouds,omitempty"`
-  HighClouds *WeatherHighClouds `xml:"highClouds,omitempty"`
-  DewpointTemperature *WeatherDewpointTemperature `xml:"dewpointTemperature,omitempty"`
+	Temperature         *WeatherTemperature         `xml:"temperature,omitempty"`
+	WindDirection       *WeatherWindDirection       `xml:"windDirection,omitempty"`
+	WindSpeed           *WeatherWindSpeed           `xml:"windSpeed,omitempty"`
+	Humidity            *WeatherHumidity            `xml:"humidity,omitempty"`
+	Pressure            *WeatherPressure            `xml:"pressure,omitempty"`
+	Cloudiness          *WeatherCloudiness          `xml:"cloudiness,omitempty"`
+	Fog                 *WeatherFog                 `xml:"fog,omitempty"`
+	LowClouds           *WeatherLowClouds           `xml:"lowClouds,omitempty"`
+	HighClouds          *WeatherHighClouds          `xml:"highClouds,omitempty"`
+	DewpointTemperature *WeatherDewpointTemperature `xml:"dewpointTemperature,omitempty"`
 }
 
 type WeatherPrecipitation struct {
-  Unit string `xml:"unit,attr"`
-  Value float32 `xml:"value,attr"`
-  MinValue float32 `xml:"minvalue,attr"`
-  MaxValue float32 `xml:"maxvalue,attr"`
+	Unit     string  `xml:"unit,attr"`
+	Value    float32 `xml:"value,attr"`
+	MinValue float32 `xml:"minvalue,attr"`
+	MaxValue float32 `xml:"maxvalue,attr"`
 }
 
 type WeatherSymbol struct {
-  Id string `xml:"id,attr"`
-  Number int `xml:"number,attr"`
+	Id     string `xml:"id,attr"`
+	Number int    `xml:"number,attr"`
 }
 
 type WeatherDewpointTemperature struct {
-  Id string `xml:"id,attr"`
-  Unit string `xml:"unit,attr"`
-  Value float32 `xml:"value,attr"`
+	Id    string  `xml:"id,attr"`
+	Unit  string  `xml:"unit,attr"`
+	Value float32 `xml:"value,attr"`
 }
 
 type WeatherHighClouds struct {
-  Id string `xml:"id,attr"`
-  Percent float32 `xml:"percent,attr"`
+	Id      string  `xml:"id,attr"`
+	Percent float32 `xml:"percent,attr"`
 }
 
 type WeatherLowClouds struct {
-  Id string `xml:"id,attr"`
-  Percent float32 `xml:"percent,attr"`
+	Id      string  `xml:"id,attr"`
+	Percent float32 `xml:"percent,attr"`
 }
 
 type WeatherFog struct {
-  Id string `xml:"id,attr"`
-  Percent float32 `xml:"percent,attr"`
+	Id      string  `xml:"id,attr"`
+	Percent float32 `xml:"percent,attr"`
 }
 
 type WeatherCloudiness struct {
-  Id string `xml:"id,attr"`
-  Percent float32 `xml:"percent,attr"`
+	Id      string  `xml:"id,attr"`
+	Percent float32 `xml:"percent,attr"`
 }
 
 type WeatherPressure struct {
-  Id string `xml:"id,attr"`
-  Unit string `xml:"unit,attr"`
-  Value float32 `xml:"value,attr"`
+	Id    string  `xml:"id,attr"`
+	Unit  string  `xml:"unit,attr"`
+	Value float32 `xml:"value,attr"`
 }
 
 type WeatherTemperature struct {
-  Id string `xml:"id,attr"`
-  Unit string `xml:"unit,attr"`
-  Value float32 `xml:"value,attr"`
+	Id    string  `xml:"id,attr"`
+	Unit  string  `xml:"unit,attr"`
+	Value float32 `xml:"value,attr"`
 }
 
 type WeatherWindDirection struct {
-  Id string `xml:"id,attr"`
-  Degrees float32 `xml:"deg,attr"`
-  Name string `xml:"name,attr"`
+	Id      string  `xml:"id,attr"`
+	Degrees float32 `xml:"deg,attr"`
+	Name    string  `xml:"name,attr"`
 }
 
 type WeatherWindSpeed struct {
-  Id string `xml:"id,attr"`
-  MilesPerSecond float32 `xml:"mps,attr"`
-  Beaufort int `xml:"beaufort,attr"`
-  Name string `xml:"name,attr"`
+	Id             string  `xml:"id,attr"`
+	MilesPerSecond float32 `xml:"mps,attr"`
+	Beaufort       int     `xml:"beaufort,attr"`
+	Name           string  `xml:"name,attr"`
 }
 
 type WeatherHumidity struct {
-  Value float32 `xml:"value,attr"`
-  Unit string `xml:"unit,attr"`
+	Value float32 `xml:"value,attr"`
+	Unit  string  `xml:"unit,attr"`
 }
 
 // Adds a method to WeatherTime returning URL for weather symbol
 // corresponding to the forecast at the time and place
 func (wt *WeatherTime) SymbolURL() (string, error) {
 
-  // Have to test since Symbol is a struct pointer
-  if(wt.Location.Symbol == nil) {
-    return "", errors.New("No Location.Symbol")
-  }
+	// Have to test since Symbol is a struct pointer
+	if wt.Location.Symbol == nil {
+		return "", errors.New("No Location.Symbol")
+	}
 
-  // Figure out if its night time at the Location
-  sunset := astrotime.CalcSunset(time.Now(), wt.Location.Latitude, wt.Location.Longitude)
-  var isNight bool = time.Since(sunset) >= 0
+	// Figure out if its night time at the Location
+	sunset := astrotime.CalcSunset(time.Now(), wt.Location.Latitude, wt.Location.Longitude)
+	var isNight bool = time.Since(sunset) >= 0
 
-  // TODO: Figure out if the Location is in polar night time
-  isPolarnight := false
-  var contentType string = "image/svg"
+	// TODO: Figure out if the Location is in polar night time
+	isPolarnight := false
+	var contentType string = "image/svg"
 
-  return WeatherIcon(wt.Location.Symbol.Number, contentType, isNight, isPolarnight), nil
+	return WeatherIcon(wt.Location.Symbol.Number, contentType, isNight, isPolarnight), nil
 }
 
 // Returns relevant WeatherTime structs for the given time
-func (wd *WeatherData) TimeForecast(time time.Time) ([]WeatherTime) {
-  var weatherTimes []WeatherTime
-  for _, weatherTime := range wd.Product.WeatherTimes {
-    if(weatherTime.Type != "forecast") {
-      continue
-    }
-    toRange := weatherTime.To.Sub(time)
-    fromRange := weatherTime.From.Sub(time)
-    if(fromRange.Hours() <= 24 && toRange.Hours() <= 1) {
-      weatherTimes = append(weatherTimes, weatherTime)
-    }
-  }
+func (wd *WeatherData) TimeForecast(time time.Time) []WeatherTime {
+	var weatherTimes []WeatherTime
+	for _, weatherTime := range wd.Product.WeatherTimes {
+		if weatherTime.Type != "forecast" {
+			continue
+		}
+		toRange := weatherTime.To.Sub(time)
+		fromRange := weatherTime.From.Sub(time)
+		if fromRange.Hours() <= 24 && toRange.Hours() <= 1 {
+			weatherTimes = append(weatherTimes, weatherTime)
+		}
+	}
 
-  return weatherTimes
+	return weatherTimes
 }
 
 // API methods
 func WeatherIcon(symbol int, contentType string, isNight bool, isPolarNight bool) string {
-  night := 0
-  if(isNight) {
-    night = 1
-  }
-  polarNight := 0
-  if(isPolarNight) {
-    polarNight = 1
-  }
-  url := fmt.Sprintf(
-    "http://api.yr.no/weatherapi/weathericon/1.1/?symbol=%d;is_night=%d;is_polarnight=%d;content_type=%s",
-    symbol, night, polarNight, contentType)
-  return url
+	night := 0
+	if isNight {
+		night = 1
+	}
+	polarNight := 0
+	if isPolarNight {
+		polarNight = 1
+	}
+	url := fmt.Sprintf(
+		"http://api.met.no/weatherapi/weathericon/1.1/?symbol=%d;is_night=%d;is_polarnight=%d;content_type=%s",
+		symbol, night, polarNight, contentType)
+	return url
 }
 
 func LocationforecastLTS(lat float64, lng float64) (WeatherData, error) {
-  // Query the external API
-  url := fmt.Sprintf("http://api.yr.no/weatherapi/locationforecastlts/1.2/?lat=%3.5f;lon=%3.5f", lat, lng)
+	// Query the external API
+	url := fmt.Sprintf("http://api.met.no/weatherapi/locationforecastlts/1.3/?lat=%3.5f;lon=%3.5f", lat, lng)
 
-  resp, err := http.Get(url)
-  if err != nil {
-    return WeatherData{}, err
-  }
+	resp, err := http.Get(url)
+	if err != nil {
+		return WeatherData{}, err
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  // Parse the XML
-  var wd WeatherData
-  err = xml.NewDecoder(resp.Body).Decode(&wd)
-  if err != nil {
-    return WeatherData{}, err
-  }
+	// Parse the XML
+	var wd WeatherData
+	err = xml.NewDecoder(resp.Body).Decode(&wd)
+	if err != nil {
+		return WeatherData{}, err
+	}
 
-  return wd, nil
+	return wd, nil
 }
-
